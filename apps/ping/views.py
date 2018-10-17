e6c4643acd1254d4ba0c9517c7f9bac0353cad3b
root <root@cptr450.cs.wallawalla.edu>
2018-10-16 17:04:46 -0700

    django server works!

--- apps/ping/views.py
+++ apps/ping/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Pong!")
 
 class Ping(APIView):
     """
