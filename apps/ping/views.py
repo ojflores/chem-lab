a5ff8345e356a4e2744ea7b1b09048d59e2ae95c
lockbo <donald.locke@wallawalla.edu>
2018-10-17 14:15:35 -0700

    env tweaks

--- apps/ping/views.py
+++ apps/ping/views.py
@@ -1,14 +1,4 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Pong!")
-
-class Ping(APIView):
-    """
-    Ping sample endpoint.
-    """
-    @staticmethod
-    def get(request):
-        return Response({'result': 'pong!'})
