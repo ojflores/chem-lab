534ecf9962d70a056da94699bd6113020afe399f
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2018-10-19 16:55:22 +0000

    Removed apps folder. Added small model to extend user.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model)
+    wwuid = models.CharField(max_length=7)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
