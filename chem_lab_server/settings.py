7d74269939b48f134d69f42b2d0fa2fe305e6721
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2018-10-22 16:19:03 +0000

    Updated database name.

--- chem_lab_server/settings.py
+++ chem_lab_server/settings.py
@@ -44,7 +44,7 @@ USE_TZ = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
+        'NAME': 'chemlab',
         'USER': os.getenv('MYSQL_USER', 'root'),
         'PASSWORD': os.getenv('MYSQL_PASSWORD', 'root'),
         'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
