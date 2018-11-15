16041e4f9de1d08fad377d3a1a22e5f88a451c06
Jeffrey Berglund <jeffrey.berglund@wallawalla.edu>
2018-11-14 18:08:43 -0800

    Update __init__.py

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -2,3 +2,5 @@ from .serializer_course import *
 from .serializer_intructor import *
 from .serializer_labgroup import *
 from .serializer_student import *
+from .serializer_assignment import *
+from .serializer_assignmenttemplate import *
