2be9d03deab1cad6cfdf099b290cd7d18bdc7706
Scylas <milwaukee.davis@wallawalla.edu>
2018-11-26 19:24:02 -0800

    Added Task Templates and updated related files.

---
+++ api/serializers/serializer_task_template.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from api.models import TaskTemplate
+
+
+class TaskTemplateSerializer(serializers.ModelSerializer):
+    """
+    The serializer for Template Tasks.
+    """
+    class Meta:
+        model = TaskTemplate
+        fields = (
+            'assignment_template',
+            'name',
+            'summary',
+            'prompt',
+            'prompt_format',
+            'image_urls',
+            'attempts_allowed',
+            'numeric_accuracy',
+            'numeric_only',
+        )
