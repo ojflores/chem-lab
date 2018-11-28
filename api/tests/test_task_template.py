980927177e053b7df28c9ca0e110b8c37baede0c
Scylas <milwaukee.davis@wallawalla.edu>
2018-11-28 15:55:06 -0800

    Completed remaining tests for task as-is, waiting on argument transition for finalizing.

--- api/tests/test_task_template.py
+++ api/tests/test_task_template.py
@@ -41,7 +41,7 @@ class TaskTemplateLCTest(APITestCase):
         self.course = Course(name="TestCourse")
         self.course.save()
 
-        # make a fake assignmnet_template
+        # make a fake assignment_template
         self.assignment_template = AssignmentTemplate(course=self.course, name="TestAssignmentTemplate")
         self.assignment_template.save()
 
@@ -142,76 +142,121 @@ class TaskTemplateLCTest(APITestCase):
         self.assertEqual(response_body['task_templates'][1]['numeric_only'], task_templates[0].numeric_only)
 
 
-class TemplateRUDTest(APITestCase):
+class TaskTemplateRUDTest(APITestCase):
     """
-    Test cases for retrieve, update, and destroy requests on TemplateRUDView.
+    Test cases for retrieve, update, and destroy requests on TaskTemplateRUDView.
     """
     def setUp(self):
         # create test user with permissions
         self.username = 'test'
         self.password = 'test'
         self.user = User.objects.create_user(username=self.username, password=self.password)
-        self.user.user_permissions.add(Permission.objects.get(codename='change_assignmenttemplate'))
-        self.user.user_permissions.add(Permission.objects.get(codename='delete_assignmenttemplate'))
+        self.user.user_permissions.add(Permission.objects.get(codename='change_tasktemplate'))
+        self.user.user_permissions.add(Permission.objects.get(codename='delete_tasktemplate'))
         self.client.login(username=self.username, password=self.password)
         # add courses and or templates to database
-        self.course = Course(name="testcourse")
+        self.course = Course(name="Astrophysics 820")
         self.course.save()
-        self.template_1 = AssignmentTemplate(name='test name 1', course=self.course)
-        self.template_1.save()
-        self.template_2 = AssignmentTemplate(name='test name 2', course=self.course)
+        self.template = AssignmentTemplate(name='Gravitational Vortices', course=self.course)
+        self.template.save()
+        self.template_2 = AssignmentTemplate(name='Cosmic Drift', course=self.course)
         self.template_2.save()
-        self.template_3 = AssignmentTemplate(name='test name 3', course=self.course)
-        self.template_3.save()
-        # retrieve the view
-        self.view_name = 'api:template-rud'
 
-    def test_template_retrieve(self):
+        self.task_template_1 = TaskTemplate(assignment_template=self.template, name='Black Holes Q1',
+                                            summary='This is a summary.', prompt='This is the prompt1',
+                                            prompt_format='CAF-citation', image_urls='all', attempts_allowed=1,
+                                            numeric_accuracy=1, numeric_only=False)
+        self.task_template_1.save()
+        self.task_template_2 = TaskTemplate(assignment_template=self.template, name='Black Holes Q2',
+                                            summary='This is a summary.', prompt='This is the prompt2',
+                                            prompt_format='CAF-citation', image_urls='all', attempts_allowed=2,
+                                            numeric_accuracy=2, numeric_only=False)
+        self.task_template_2.save()
+        self.task_template_3 = TaskTemplate(assignment_template=self.template, name='Black Holes Q3',
+                                            summary='This is a summary.', prompt='This is the prompt3',
+                                            prompt_format='SIGFIGS', image_urls='all', attempts_allowed=3,
+                                            numeric_accuracy=3, numeric_only=True)
+        self.task_template_3.save()
+
+
+        # retrieve the view
+        self.view_name = 'api:task_template-rud'
+
+    def test_task_template_retrieve(self):
         """
-        Tests that a assignment template is properly retrieved.
+        Tests that a task template is properly retrieved.
         """
         # request
-        response = self.client.get(reverse(self.view_name, args=[self.template_2.id]))
+        response = self.client.get(reverse(self.view_name, args=[self.task_template_1.id]))
         response_body = json.loads(response.content.decode('utf-8'))
         # test response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response_body['pk'], self.template_2.id)
-        self.assertEqual(response_body['name'], self.template_2.name)
-        self.assertEqual(response_body['course'], self.course.id)
+        self.assertEqual(response_body['pk'], self.task_template_1.id)
+        self.assertEqual(response_body['assignment_template'], self.task_template_1.assignment_template.id)
+        self.assertEqual(response_body['name'], self.task_template_1.name)
+        self.assertEqual(response_body['summary'], self.task_template_1.summary)
+        self.assertEqual(response_body['prompt'], self.task_template_1.prompt)
+        self.assertEqual(response_body['prompt_format'], self.task_template_1.prompt_format)
+        self.assertEqual(response_body['image_urls'], self.task_template_1.image_urls)
+        self.assertEqual(response_body['attempts_allowed'], self.task_template_1.attempts_allowed)
+        self.assertEqual(response_body['numeric_accuracy'], self.task_template_1.numeric_accuracy)
+        self.assertEqual(response_body['numeric_only'], self.task_template_1.numeric_only)
 
-    def test_template_update(self):
+    def test_task_template_update(self):
         """
-        Tests that a template is properly updated.
+        Tests that a task template is properly updated.
         """
         # modify values
         request_body = {
+            'assignment_template': self.template_2.id,
             'name': 'name changed',
-            'course': self.course.id
+            'summary': 'new summary',
+            'prompt': 'new prompt',
+            'prompt_format': 'CAC-Citation',
+            'image_urls': 'none',
+            'attempts_allowed': 64,
+            'numeric_accuracy': 64,
+            'numeric_only': True,
         }
         # request
-        response = self.client.put(reverse(self.view_name, args=[self.template_2.id]), request_body)
+        response = self.client.put(reverse(self.view_name, args=[self.task_template_2.id]), request_body)
         response_body = json.loads(response.content.decode('utf-8'))
         # test database
-        assignment_template = AssignmentTemplate.objects.filter(name=request_body['name'], course=request_body['course']).first()
-        self.assertEqual(assignment_template.id, self.template_2.id)
-        self.assertEqual(assignment_template.course.id, request_body['course'])
-        self.assertEqual(assignment_template.name, request_body['name'])
+        task_template = TaskTemplate.objects.filter(name=request_body['name']).first()
+        self.assertEqual(task_template.id, self.task_template_2.id)
+        self.assertEqual(task_template.name, request_body['name'])
+        self.assertEqual(task_template.summary, request_body['summary'])
+        self.assertEqual(task_template.prompt, request_body['prompt'])
+        self.assertEqual(task_template.prompt_format, request_body['prompt_format'])
+        self.assertEqual(task_template.image_urls, request_body['image_urls'])
+        self.assertEqual(task_template.attempts_allowed, request_body['attempts_allowed'])
+        self.assertEqual(task_template.numeric_accuracy, request_body['numeric_accuracy'])
+        self.assertEqual(task_template.numeric_only, request_body['numeric_only'])
+
         # test response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response_body['pk'], self.template_2.id)
-        self.assertEqual(response_body['course'], request_body['course'])
+        self.assertEqual(response_body['assignment_template'], request_body['assignment_template'])
         self.assertEqual(response_body['name'], request_body['name'])
+        self.assertEqual(response_body['summary'], request_body['summary'])
+        self.assertEqual(response_body['prompt'], request_body['prompt'])
+        self.assertEqual(response_body['prompt_format'], request_body['prompt_format'])
+        self.assertEqual(response_body['image_urls'], request_body['image_urls'])
+        self.assertEqual(response_body['attempts_allowed'], request_body['attempts_allowed'])
+        self.assertEqual(response_body['numeric_accuracy'], request_body['numeric_accuracy'])
+        self.assertEqual(response_body['numeric_only'], request_body['numeric_only'])
 
-    def test_template_destroy(self):
+
+    def test_task_template_destroy(self):
         """
-        Tests that a template is properly destroyed.
+        Tests that a task template is properly destroyed.
         """
         # request
-        response = self.client.delete(reverse(self.view_name, args=[self.template_2.id]))
+        response = self.client.delete(reverse(self.view_name, args=[self.task_template_2.id]))
         # test database
-        assignment_templates = AssignmentTemplate.objects.all()
-        self.assertTrue(self.template_1 in assignment_templates)
-        self.assertTrue(self.template_2 not in assignment_templates)
-        self.assertTrue(self.template_3 in assignment_templates)
+        task_templates = TaskTemplate.objects.all()
+        self.assertTrue(self.task_template_1 in task_templates)
+        self.assertTrue(self.task_template_2 not in task_templates)
+        self.assertTrue(self.task_template_3 in task_templates)
         # test response
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
