c8ea6b0ab901d9c6e69729151f7b80eeb3d4ffd6
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2018-12-06 21:10:47 -0800

    Assignment tempalates are only shown when users have an associated assignment open to them.

--- api/views/view_assignment_template.py
+++ api/views/view_assignment_template.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import DjangoModelPermissions
 
+from datetime import datetime
+from pytz import timezone
+
 from api import serializers
-from api.models import AssignmentTemplate
+from api.models import AssignmentTemplate, Assignment, Student
 from api.permissions import IsStudentOrInstructor
 
 
@@ -15,6 +19,17 @@ class AssignmentTemplateLCView(ListCreateAPIView):
     permission_classes = (DjangoModelPermissions, IsStudentOrInstructor)
 
     def get_queryset(self):
+        if self.request.user.groups.filter(name='Student').exists():
+            # get student
+            student = Student.objects.get(user=self.request.user)
+            # get all assignments open to the user
+            current_time = datetime.now(timezone(settings.TIME_ZONE))
+            assignments = Assignment.objects.filter(labgroup=student.labgroup,
+                                                    open_date__lt=current_time,
+                                                    close_date__gt=current_time).all()
+            # get "open" assignment templates from the students course
+            return AssignmentTemplate.objects.filter(
+                assignment__in=assignments, course=student.labgroup.course).all()
         return AssignmentTemplate.objects.all()
 
     # returns a list of AssignmentTemplates
